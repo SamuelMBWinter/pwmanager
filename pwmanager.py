24afbda35cd378b45363310aa47f363e381e0cc5
Samuel M B Winter <Samuel.M.B.Winter@protonmail.com>
2021-02-08 17:40:33 +0000

    Adds a dummy acount to te database

--- pwmanager.py
+++ pwmanager.py
@@ -2,7 +2,7 @@ import sqlite3
 
 # Creating an `Account` object to store the data associated with an account
 class Account:
-    def __init__(self, service, username password, email = None, url = None)
+    def __init__(self, service, username, password, email = None, url = None):
         self.service = service
         self.usr = username
         self.pwd = password
@@ -11,8 +11,8 @@ class Account:
 
 
 # Create/ Connect to a database, and create a cursor to execute commands
-conn = sqlite3.connect("memory")
-cur = con.cusor()
+conn = sqlite3.connect(":memory:")
+cur = conn.cursor()
 
 cur.execute("""
     CREATE TABLE pwds(
@@ -24,5 +24,25 @@ cur.execute("""
     )
 """)
 
-Acc1 = Account("sevice1", "samuelmbwinter", "A very strong password")
+Acc1 = Account("service1", "samuelmbwinter", "A very strong password")
 
+cur.execute(
+    "INSERT INTO pwds VALUES (:ser, :usr, :pwd, :email, :url)",
+    {
+        'ser': Acc1.service,
+        'usr': Acc1.usr,
+        'pwd': Acc1.pwd,
+        'email': Acc1.email,
+        'url': Acc1.url,
+    }
+)
+conn.commit()
+
+cur.execute("SELECT * FROM pwds WHERE service='service1'")
+print(cur.fetchall())
+
+cur.close
+
+conn.commit()
+
+conn.close()
