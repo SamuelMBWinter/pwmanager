9b7f2a937f8b4b4a29f5e8f3623d8af29a03e4ad
Samuel M B Winter <Samuel.M.B.Winter@protonmail.com>
2021-02-08 21:42:00 +0000

    Restructured program to better manage opening and closing of the database

--- pwmanager.py
+++ pwmanager.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-# Creating an `Account` object to store the data associated with an account
+# Creating an `Account` class to store the data associated with an account
 class Account:
     def __init__(self, service, username, password, email = None, url = None):
         self.service = service
@@ -9,40 +9,60 @@ class Account:
         self.email = email
         self.url = url
 
+# SQLite3 `Safe` class to manage the data base - this helps remove biolerplate code
+class Safe:
+    def __init__(self, path):
+        self.path = path
 
-# Create/ Connect to a database, and create a cursor to execute commands
-conn = sqlite3.connect(":memory:")
-cur = conn.cursor()
+    def __enter__(self):
+        self.connection = sqlite3.connect(self.path)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("""
+            CREATE TABLE pwds(
+            service text,
+            username text,
+            password text,
+            email text,
+            url text
+            )
+        """)
+        
+        return self
 
-cur.execute("""
-    CREATE TABLE pwds(
-    service text,
-    username text,
-    password text,
-    email text,
-    url text
-    )
-""")
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+        self.connection.close()
+
+    def add_acc(self, account):
+        self.cursor.execute("""
+            INSERT INTO pwds VALUES (:ser, :usr, :pwd, :email, :url)
+            """,
+            {
+                'ser': Acc1.service,
+                'usr': Acc1.usr,
+                'pwd': Acc1.pwd,
+                'email': Acc1.email,
+                'url': Acc1.url,
+            }
+        )
+    
+    def retrieve_acc(self, service=None, username=None):
+        self.cursor.execute("""
+            SELECT * FROM pwds WHERE service=:service OR username=:username
+            """,
+            {
+                'service': service,
+                'username': username,
+            }
+        )
+        return self.cursor.fetchall()
 
 Acc1 = Account("service1", "samuelmbwinter", "A very strong password")
 
-cur.execute(
-    "INSERT INTO pwds VALUES (:ser, :usr, :pwd, :email, :url)",
-    {
-        'ser': Acc1.service,
-        'usr': Acc1.usr,
-        'pwd': Acc1.pwd,
-        'email': Acc1.email,
-        'url': Acc1.url,
-    }
-)
-conn.commit()
+db_path = ":memory:"
 
-cur.execute("SELECT * FROM pwds WHERE service='service1'")
-print(cur.fetchall())
-
-cur.close
-
-conn.commit()
-
-conn.close()
+with Safe(db_path) as pwds:
+    pwds.add_acc(Acc1) 
+    pwds.cursor.execute("SELECT * FROM pwds WHERE service='service1'")
+    print(pwds.retrieve_acc(service="service1"))
+    
